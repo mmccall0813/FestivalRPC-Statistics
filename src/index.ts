b8278b61a51aa357938659a1353e3d6d858ed3bb
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2024-10-26 02:53:48 -0400

    save the data somewhere

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import { readFileSync } from "fs";
+import fs from "fs";
 import socketio from "socket.io";
 
-const config = JSON.parse(readFileSync("./config.json").toString());
+const config = JSON.parse(fs.readFileSync("./config.json").toString());
 const io = new socketio.Server();
 let debuglogs = true;
+
+interface SongPlay {
+    id: string,
+    instrument: string,
+    date: number,
+    duration: number,
+    difficulty: string
+}
+
+let data: {[key: string]: {plays: SongPlay[], timeplayed: number, instruments: {[key: string]: number}}} = {};
+
+if(!fs.existsSync("./data/data.json")) data = JSON.parse(fs.readFileSync("./data/data.json").toString());
 
 io.on("connection", (socket) => {
     let identified = false;
@@ -21,10 +33,12 @@ io.on("connection", (socket) => {
     socket.on("identify", (name: string) => {
         if(typeof name !== "string") return socket.disconnect(); // stop trying to crash my server!!!
         id = name;
+        identified = true;
     });
 
     socket.on("startSong", (song: string, instrument: string, difficulty: string) => {
         if(typeof song !== "string" || typeof instrument !== "string" || typeof difficulty !== "string") return socket.disconnect(); // stop trying to crash my server!!!
+        if(debuglogs) console.log("recieved startsong");
         status.playing = true;
         status.song = song;
         status.difficulty = difficulty;
@@ -32,6 +46,31 @@ io.on("connection", (socket) => {
         status.started = Date.now();
     })
 
+    socket.on("stopSong", () => {
+        status.playing = false;
+
+        if(!data[status.song]){
+            data[status.song] = {plays: [], instruments: {}, timeplayed: 0};
+        };
+
+        data[status.song].plays.push(
+            {
+                "date": Date.now(),
+                "difficulty": status.difficulty,
+                "duration": Math.floor((Date.now() - status.started) / 1000),
+                "instrument": status.instrument,
+                "id": id
+            }
+        );
+
+        data[status.song].timeplayed += Math.floor((Date.now() - status.started) / 1000);
+        if(!data[status.song].instruments[status.instrument]) data[status.song].instruments[status.instrument] = 0;
+        data[status.song].instruments[status.instrument]++;
+
+        fs.writeFileSync("./data/data.json", JSON.stringify(data, null, 2));
+
+        if(debuglogs) console.log(id + " just finished playing " + status.song + " and spent " + ((Date.now() - status.started) / 1000) + " seconds playing it.")
+    })
     
     setTimeout( () => {
         if(!identified) socket.disconnect();
